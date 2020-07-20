8cdeb49ddc464d05852c782d511d73dc909d21ed
root <emaveric@mi-a1.msk.21-school.ru>
2020-07-20 21:20:54 +0300

    add ft_printf, nothing else

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -1,11 +1,30 @@
-//
-// Created by Erik Maverick on 7/20/20.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem-in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emaveric <emaveric@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/20 20:07:40 by emaveric          #+#    #+#             */
+/*   Updated: 2020/07/20 20:50:12 by emaveric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef LEM_IN_H
 # define LEM_IN_H
 
+# define MAX_INT 2147483647
+# define ERROR -1
+
+
 # include "../ft_printf/libft/libft.h"
 # include "../ft_printf/includes/ft_printf.h"
+
+typedef struct	s_lem_in
+{
+	int 		ant_num;
+	int 		ant_start;
+	int 		ant_end;
+}				t_lem_in;
 
 #endif
